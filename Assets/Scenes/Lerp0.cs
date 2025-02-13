f30171260b7784f8d98358b7d3ecd833cac80db5
andiesjaa <krookshanks04@gmail.com>
2025-02-12 21:19:44 -0500

    Beautiful heart try 2

--- Assets/Scenes/Lerp0.cs
+++ Assets/Scenes/Lerp0.cs
@@ -28,14 +28,19 @@ public class Lerp0 : MonoBehaviour
         endPosition = new Vector3[numSphere]; 
         
         // Define target positions. Start = random, End = heart 
+        float t = 0;
         for (int i =0; i < numSphere; i++){
             // Random start positions
             float r = 10f;
-            startPosition[i] = new Vector3(r * Random.Range(-1f, 1f), r * Random.Range(-1f, 1f), r * Random.Range(-1f, 1f));        
+            startPosition[i] = new Vector3(r * Random.Range(-1f, 1f), r * Random.Range(-1f, 1f), r * Random.Range(-1f, 1f));     
+            //those 3 are the x and y parameters   
 
             r = 3f; // radius of the circle
             // Circular end position
-            endPosition[i] = new Vector3(r * Mathf.Sin(i * 2 * Mathf.PI / numSphere), r * Mathf.Cos(i * 2 * Mathf.PI / numSphere));
+            endPosition[i] = new Vector3(Mathf.Pow(2f, 0.5f)*Mathf.Pow(Mathf.Sin(t), 3f), (-1f*Mathf.Pow(Mathf.Cos(t), 3f)) - Mathf.Pow(Mathf.Cos(t), 2f) + (2f*Mathf.Cos(t)));
+            //x = r * Mathf.Sin(i * 2 * Mathf.PI / numSphere)
+            // y = r * Mathf.Cos(i * 2 * Mathf.PI / numSphere
+            t += 0.1f;
         }
         // Let there be spheres..
         for (int i =0; i < numSphere; i++){
@@ -81,7 +86,8 @@ public class Lerp0 : MonoBehaviour
             // Color Update over time
             Renderer sphereRenderer = spheres[i].GetComponent<Renderer>();
             float hue = (float)i / numSphere; // Hue cycles through 0 to 1
-            Color color = Color.HSVToRGB(Mathf.Abs(hue * Mathf.Sin(time)), Mathf.Cos(time), 2f + Mathf.Cos(time)); // Full saturation and brightness
+            hue = 0.25f*hue + 0.75f;
+            Color color = Color.HSVToRGB(Mathf.Abs(hue), 1, 1); // Full saturation and brightness
             sphereRenderer.material.color = color;
         }
     }
